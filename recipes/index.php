6884d5f0f76faa55bc6a7dd1df92421a84c499d7
cmi112 <cabdiqanimuuse7@gmail.com>
2021-12-01 10:10:05 +0100

    table data created data insert and get done with db

--- recipes/index.php
+++ recipes/index.php
@@ -1,10 +1,20 @@
 <?php
-$msg="";
-// If upload button is pressed
-if(isset($_POST['upload'])){
+
+$db = mysqli_connect("localhost","root","root","posts");  // database connection
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+<?php
+
+
+
+if(isset($_POST["submit"]))
+{
   $target="recipes/images/".basename($_FILES['image']['name']);
-  // Connect db
-  $db=mysqli_connect("localhost","root","root","recipes");
   // Get all the subitted data from the form
   $image=$_FILES['image']['name'];
   $text=$_POST['text'];
@@ -18,28 +28,88 @@ if(isset($_POST['upload'])){
   }else{
     $msg ="Failed";
   }
+  
 }
-
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fetch image from database in PHP</title>
+  <style>
+  table img{
+    width:100px; 
+    height:100px;
+   
+  }
+  tr{
+    border:2px solid;
+    
+  }
+  td {
+    border:2px solid;
+    padding:10px;
 
-<?php 
- // Connect db
- $db=mysqli_connect("localhost","root","root","recipes");
- $sql="SELECT * FROM images";
- $result=mysqli_query($db,$sql);
- while($row=mysqli_fetch_array($result)){
-   echo "<div>";
-   echo "<img src='recipes/images/".$row['image']."'>";
-   echo "<p>".$row['text']."</p>";
-   echo "</div>";
- }
+  }
+  
+  </style>
+</head>
+<body>
 
-?>
+<h2>Insert Data</h2>
 
 <form action="index.php?name=recipes" method="post" enctype="multipart/form-data">
-   <input type="hidden" name="size" value="100000">
-    <input type="file" name="image">
-    <textarea name="text" id="" cols="30" rows="10"></textarea>
-    <input type="submit" name="upload" value="Upload Image">
+  <table border="2">
+    <tr>
+      <td>Enter Name</td>
+      <td><input type="text" name="text" placeholder="Enter Name" Required></td>
+    </tr>
+    <tr>
+      <td>Select Image</td>
+      <td><input type="file" name="image" Required></td>
+    </tr>
+    <tr>
+      <td colspan="2"><input type="submit" name="submit" value="Upload"></td>			
+    </tr>
+  </table>
 </form>
+
+<hr/>
+
+
+
+
+<h2>All Records</h2>
+
+<table border="2">
+  <tr>
+    <td>ID</td>
+    <td>Text</td>
+    <td>Images</td>
+  </tr>
+
+<?php
+
+
+
+$records = mysqli_query($db,"select * from images"); // fetch data from database
+
+while($data = mysqli_fetch_array($records))
+{
+?>
+  <tr>
+    <td><?php echo $data['id']; ?></td>
+    <td><?php echo $data['text']; ?></td>
+    <!-- <td><img src="<?php echo $data['images']; ?>" width="100" height="100" ></td> -->
+   <td><?php echo "<img  src='recipes/images/".$data['image']. "'>";?></td>
+  </tr>	
+<?php
+}
+?>
+
+</table>
+
+<?php mysqli_close($db);  // close connection ?>
+
+</body>
+</html>
